8ee8eaeda6de64adee16ef9cd723335239cf9aa0
Coffi <coffipants@gmail.com>
2025-12-16 13:59:24 +0100

    Implement basic in-memory todo list logic

--- app/src/main/java/com/example/wido/MainActivity.kt
+++ app/src/main/java/com/example/wido/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.example.wido
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private val todos = mutableListOf<TodoItem>()
+    private var counter = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val addButton = findViewById<Button>(R.id.addTodoButton)
+        val todoText = findViewById<TextView>(R.id.todoListText)
+
+        addButton.setOnClickListener {
+            val todo = TodoItem(
+                id = counter.toLong(),
+                title = "Todo $counter"
+            )
+            counter++
+            todos.add(todo)
+
+            todoText.text = todos.joinToString("\n") { it.title }
+        }
     }
 }
